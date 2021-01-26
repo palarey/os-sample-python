197b4fdb4ccd5e2f02073f63b699d2edf1f9f3b1
Tim Cook <tlcook@me.com>
2021-01-26 13:20:57 +0000

    added request import

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    bic = request.args.get("bic")
+    bic = request.args.get("bic", "<none>")
     return "Hello New/Modified World! bic: {}".format(bic)
 
 if __name__ == "__main__":
