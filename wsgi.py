0ec83536190cb0e61b34bb72b0f793f117cb44c7
Tim Cook <tlcook@me.com>
2021-01-26 14:22:12 +0000

    added json response

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 application = Flask(__name__)
+
+# Map BIC onto project
+MAPPER = {
+    "BIC1": "projectA",
+    "BIC2": "projectB",
+    "BIC3": "projectC",
+    "default": "default-project"
+}
 
 @application.route("/")
 def hello():
-    bic = request.args.get("bic", "<none>")
-    return "Hello New/Modified World! bic: {}".format(bic)
+    bic = request.args.get("bic", "")
+
+    project = MAPPER.get(bic, MAPPER.get("default", ""))
+
+    resp = {"status": "ok", "err": "", "bic": bic, "project": project}
+
+    return jsonify(resp)
 
 if __name__ == "__main__":
     application.run()
